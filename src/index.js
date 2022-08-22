a62127ca12184a8ad36478576c6d438dd87a01f4
Gacheruian <ian.gacheru@student.moringaschool.com>
2022-08-22 23:00:37 +0300

    Done the assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+let form = document.getElementById('create-task-form')
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  handleToDo(e.target.new.value)
+  form.reset()
+
+})
 });
+
+function handleToDo(toDo)
+{
+  console.log(toDo)
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.textContent = `${toDo} `
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#list').appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
